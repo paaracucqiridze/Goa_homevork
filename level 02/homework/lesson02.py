6d2bd84013153fcd3889a85654c355ca3016d64a
paaracucqiridze <paatacucqiridze002@gmail.com>
2024-07-22 22:21:40 +0400

    commit

---
+++ level 02/homework/lesson02.py
@@ -0,0 +1,7 @@
+# print("paata" + "cucqiridze")
+# print
+num = 13
+num1 = "cxoveli"
+safe = 9
+saxeli = "paata" 
+print(safe + saxeli)
---
+++ level 03/homework/lesson03.py
@@ -0,0 +1,6 @@
+name = "paata"
+surname = "cucqiridze"
+hob = "football"
+country = "America"
+age = 13
+print(name, surname, hob, country, age )
